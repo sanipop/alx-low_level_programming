366378f87683ad60e750400345901e286bd56f26
sanipop <thepersian82@gmail.com>
2023-07-13 11:59:18 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /*
- * A printf c program
+ *main - A printf c program
+ *Return: 0 success
  */
-int main(void) {
+int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
 
