e7dab8bf23cd881ad464b6c12f0d226670011cca
sanipop <thepersian82@gmail.com>
2023-07-13 11:25:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
-int main(void)
 /**
  * main - on
  *
  * Return: Value 1
- */	
+ */
+int main(void)	
 {
 	puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	return (0);
 }
+
