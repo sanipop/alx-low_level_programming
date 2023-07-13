dfe33d9ea3a9e1a69e9d7bd766415ec1eaea8b19
sanipop <thepersian82@gmail.com>
2023-07-13 13:24:33 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,6 @@ printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of float: %lu byte(s)\n",(unsigned long)sizeof(f))
+printf("Size of float: %lu byte(s)\n",(unsigned long)sizeof(f));
     return (0);
 }
