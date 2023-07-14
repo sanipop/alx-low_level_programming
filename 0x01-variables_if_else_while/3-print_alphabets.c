1be50a554a9498e0b9f734dafaee9b6ed1f68486
sanipop <thepersian82@gmail.com>
2023-07-14 15:38:50 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    char lowercase;
+    char uppercase;
+
+    lowercase = 'a';
+    uppercase = 'A';
+
+    while (lowercase <= 'z')
+    {
+        putchar(lowercase);
+        lowercase++;
+    }
+
+    while (uppercase <= 'Z')
+    {
+        putchar(uppercase);
+        uppercase++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
+
