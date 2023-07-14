b26bb2082c5ae4243da7adbe90c7af8b3dc61f5c
sanipop <thepersian82@gmail.com>
2023-07-14 12:37:10 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
     int n;
@@ -12,14 +17,14 @@ int main(void)
 
     lastDigit = abs(n) % 10;
 
-    printf("Last digit of %d is %d", n, lastDigit);
+    printf("Last digit of %d is %d and is ", n, lastDigit);
 
-    if (lastDigit < 6 && lastDigit != 0)
-        printf(" and is less than 6 and not 0\n");
-    else if (lastDigit > 5)
-        printf(" and is greater than 5\n");
+    if (lastDigit > 5)
+        printf("greater than 5\n");
+    else if (lastDigit == 0)
+        printf("0\n");
     else
-        printf(" and is 0\n");
+        printf("less than 6 and not 0\n");
 
     return (0);
 }
