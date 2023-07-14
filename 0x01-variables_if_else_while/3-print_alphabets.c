419a127505843ec0375f535d7d3304e6422f1fe7
sanipop <thepersian82@gmail.com>
2023-07-14 15:17:16 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char c;
+c = "a"
+while
+(c = a)
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
