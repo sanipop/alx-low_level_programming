78363305470ad72d442f3ca168458361109a1283
sanipop <thepersian82@gmail.com>
2023-07-14 17:01:52 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,33 +7,28 @@
  */
 int main(void)
 {
-int i, j;
+	int i, j;
 
-for (i = 0; i <= 99; i++)
-{
-for (j = i; j <= 99; j++)
-{
-int first_num = i % 10;
-int second_num = j % 10;
+	for (i = 0; i <= 98; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
 
-if (i / 10 < second_num || (i / 10 == second_num && first_num < j / 10))
+			if (i != 98 || j != 99)
 			{
-putchar((i / 10) + '0');
-putchar((i % 10) + '0');
-putchar(' ');
-putchar((j / 10) + '0');
-putchar((j % 10) + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-if (i != 99 || j != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
+	putchar('\n');
 
-putchar('\n');
-return 0;
+	return 0;
 }
 
