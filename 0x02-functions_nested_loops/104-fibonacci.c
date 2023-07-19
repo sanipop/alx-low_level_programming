f723194b8e78d36ecdee55c3604075c71499debb
sanipop <thepersian82@gmail.com>
2023-07-19 00:45:25 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,17 +13,21 @@ int num;
 unsigned long int fib1 = 1, fib2 = 2, next;
 
 printf("%lu, %lu", fib1, fib2);
-
 for (num = 2; num < 98; num++)
 {
 next = fib1 + fib2;
+
+if (num % 10 == 9)
+printf(",\n\t%lu", next);
+else
 printf(",\t%lu", next);
 
 fib1 = fib2;
 fib2 = next;
 }
+
 printf("\n");
 
-return (0);
+return 0;
 }
 
