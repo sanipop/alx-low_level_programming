1f1bfb60df9ac15eb153bb0a58bb56cbc77db3b7
sanipop <thepersian82@gmail.com>
2023-07-19 00:24:07 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,19 +9,19 @@
  */
 int main(void)
 {
-    int num;
-    int sum = 0;
+int num;
+int sum = 0;
 
-    for (num = 0; num < 1024; num++)
-    {
-        if (num % 3 == 0 || num % 5 == 0)
-        {
-            sum += num;
-        }
-    }
+for (num = 0; num < 1024; num++)
+{
+if (num % 3 == 0 || num % 5 == 0)
+{
+sum += num;
+}
+}
 
-    printf("%d\n", sum);
+printf("%d\n", sum);
 
-    return (0);
+return (0);
 }
 
