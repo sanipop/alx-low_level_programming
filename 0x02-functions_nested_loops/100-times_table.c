681fbcbce7e332c6e13d0ba889c10fb692f8a272
sanipop <thepersian82@gmail.com>
2023-07-19 00:13:18 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,10 +11,10 @@ void print_times_table(int n)
 int num, prod, mult;
 if (n >= 0 && n <= 15)
 {
-for (num = 0; num <= n; num ++)
+for (num = 0; num <= n; num++)
 {
 _putchar('0');
-for (mult = 1; mult <=n; mult ++)
+for (mult = 1; mult <= n; mult++)
 {
 _putchar(',');
 _putchar(' ');
@@ -30,9 +30,9 @@ _putchar(((prod / 10)) % 10 + '0');
 }
 else if (prod <= 99 && prod >= 10)
 {
-_putchar((prod / 10 ) + '0');
+_putchar((prod / 10) + '0');
 }
-_putchar((prod % 10 ) + '0');
+_putchar((prod % 10) + '0');
 }
 _putchar('\n');
 }
