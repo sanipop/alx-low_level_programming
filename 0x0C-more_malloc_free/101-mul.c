4503665253e8016443a9b3e8fba9972de3b73318
sanipop <thepersian82@gmail.com>
2023-08-10 19:20:13 -0700

    r

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,43 +5,43 @@
 #define ERR_MSG "Error"
 
 /**
- * is_digit - verify if digit
- * @s: input
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: binary output
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s)
 {
-	int x = 0;
+	int i = 0;
 
-	while (s[x])
+	while (s[i])
 	{
-		if (s[x] < '0' || s[x] > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		x++;
+		i++;
 	}
 	return (1);
 }
 
 /**
- * _strlen - find str size
- * @s: input
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: str size
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
-	int x = 0;
+	int i = 0;
 
-	while (s[x] != '\0')
+	while (s[i] != '\0')
 	{
-		x++;
+		i++;
 	}
 	return (i);
 }
 
 /**
- * errors - error handling
+ * errors - handles errors for main
  */
 void errors(void)
 {
@@ -50,22 +50,22 @@ void errors(void)
 }
 
 /**
- * main - product of input
- * @argc: input size
- * @argv: input array
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	char *q, *r;
+	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	q = argv[1], r = argv[2];
-	if (argc != 3 || !is_digit(q) || !is_digit(r))
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-	len1 = _strlen(q);
-	len2 = _strlen(r);
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
 	if (!result)
@@ -74,11 +74,11 @@ int main(int argc, char *argv[])
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = q[len1] - '0';
+		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = _strlen(r) - 1; len2 >= 0; len2--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			digit2 = r[len2] - '0';
+			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
@@ -99,4 +99,5 @@ int main(int argc, char *argv[])
 	free(result);
 	return (0);
 }
+
 
