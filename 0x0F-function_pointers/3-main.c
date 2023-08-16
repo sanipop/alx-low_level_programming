c84c7a624a3278cf194849e9a3247554aa921235
sanipop <thepersian82@gmail.com>
2023-08-16 15:48:23 -0700

    6

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,48 +3,42 @@
 #include <stdio.h>
 #include "3-calc.h"
 /**
- * main - Send output to std out.
- * @argc: number of input in array .
- * @argv: Input array from std in.
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: Always 0.
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int x, y;
-	char *a;
-
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);		}
+		exit(98);
 	}
 
-		x = atoi(argv[1]);
-		a = argv[2];
-		y = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
-
-		if (get_op_func(a) == NULL || a[1] != '\0')
-		{
-			printf("Error\n");
-			exit(99);
-		}
-
-
-		if ((*a == '/' && y == 0) ||
-		    (*a == '%' && y == 0))
-		{
-			printf("Error\n");
-			exit(100);
-		}
-
-
-		printf("%d\n", get_op_func(a)(x, y));
-
-
-		return (0);
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
 	}
 
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
+	return (0);
+}
 
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,66 +1,63 @@
 #include "3-calc.h"
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
-	int op_add(int a, int b);
-	int op_sub(int a, int b);
-	int op_mul(int a, int b);
-	int op_div(int a, int b);
-	int op_mod(int a, int b);
-
-
-	/**
-	 * op_add - Fxn for doing Addition .
-	 * @a: var 1.
-	 * @b: var 2.
-	 *
-	 * Return: Result of addition .
-	 */
-	int op_add(int a, int b)
-	{
-		return (a + b);
-	}
-	/**
-	 * op_sub - fxn for doing subtraction .
-	 * @a: var 1.
-	 * @b: var 2.
-	 *
-	 * Return: Result.
-	 */
-	int op_sub(int a, int b)
-	{
-		return (a - b);
-	}
-	/**
-	 * op_mul -fxn to multiply 2 nums
-	 * @a: var 1
-	 * @b: var 2.
-	 *
-	 * Return: Result of a x b.
-	 */
-	int op_mul(int a, int b)
-	{
-		return (a * b);
-	}
-	/**
-	 * op_div - func to div.
-	 * @a: var 1.
-	 * @b: var2.
-	 *
-	 * Return: Results for the division .
-	 */
-	int op_div(int a, int b)
-	{
-		return (a / b);
-	}
-	/**
-	 * op_mod - Function for cal the Remainder 
-	 * @a: var 1.
-	 * @b: var2.
-	 *
-	 * Return: resuld of the modules operation .
-	 */
-	int op_mod(int a, int b)
-	{
-		return (a % b);
-	}
-
+/**
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The quotient of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The remainder of the division of a by b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
