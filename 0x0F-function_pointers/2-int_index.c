6ecfeba0f7908cebd4a979a164a5df436a40e261
sanipop <thepersian82@gmail.com>
2023-08-16 14:55:27 -0700

    third

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,7 +8,7 @@
 	 */
 	int int_index(int *array, int size, int (*cmp)(int))
 	{
-		int i;
+		int k;
 
 
 		if (array == NULL || size <= 0 || cmp == NULL)
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,33 +1,32 @@
 #include "function_pointers.h"
-	#include <stdlib.h>
-	#include <stdio.h>
-	#include "3-calc.h"
-	/**
-	 * main - Send output to std out.
-	 * @argc: number of input in array .
-	 * @argv: Input array from std in.
-	 *
-	 * Return: Always 0.
-	 */
-	int main(int __attribute__((__unused__)) argc, char *argv[])
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * main - Send output to std out.
+ * @argc: number of input in array .
+ * @argv: Input array from std in.
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int x, y;
+	char *a;
+
+
+	if (argc != 4)
 	{
-		int x, y;
-		char *a;
-
-
-		if (argc != 4)
-		{
-			printf("Error\n");
-			exit(98);
-		}
-
+		printf("Error\n");
+		exit(98);		}
+	}
 
 		x = atoi(argv[1]);
 		a = argv[2];
 		y = atoi(argv[3]);
 
 
-		if (get_op_func(op) == NULL || a[1] != '\0')
+		if (get_op_func(a) == NULL || a[1] != '\0')
 		{
 			printf("Error\n");
 			exit(99);
@@ -42,7 +41,7 @@
 		}
 
 
-		printf("%d\n", get_op_func(op)(x, y));
+		printf("%d\n", get_op_func(a)(x, y));
 
 
 		return (0);
