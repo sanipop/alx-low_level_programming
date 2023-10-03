df3cfdd2cfcdff631d4d496a72c76d57e66e3863
sanipop <67932273+sanipop@users.noreply.github.com>
2023-10-03 01:58:07 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,16 +15,21 @@ int append_text_to_file(const char *filename, char *text_content)
         
         if (filename == NULL)
                 return (-1);
+        
         if (text_content != NULL)
         {
                 for (leng = 0; text_content[leng];)
                         leng++;
         }
+        
         outt = open(filename, O_WRONLY | O_APPEND);
         wri = write(outt, text_content, leng);
+        
         if (outt == -1 || wri == -1)
                 return (-1);
+        
         close(outt);
+        
         return (1);
 }
 
