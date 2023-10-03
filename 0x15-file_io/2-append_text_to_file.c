1b7d13a8049b46a52b67d299939f00ddce8a9948
sanipop <67932273+sanipop@users.noreply.github.com>
2023-10-03 01:54:25 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,10 +9,10 @@
  *         If the file does not exist the user lacks write permissions - -1.
  *         Otherwise - 1.
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
         int outt, wri, leng = 0;
+        
         if (filename == NULL)
                 return (-1);
         if (text_content != NULL)
