1a8ef7f25ef4d1370d335a8e45eea90bcb757db8
sanipop <thepersian82@gmail.com>
2023-11-13 18:30:25 -0800

    pp\::

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/sanipop/alx-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -P .. https://github.com/sanipop/alx-low_level_programming/raw/2993c7ea9126d277e72edf60e6c727943f7d92ed/0x18-dynamic_libraries/win.so
+export LD_PRELOAD="$PWD/../win.so"
