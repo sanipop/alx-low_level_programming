3850c354665475916c2289be19ae92f42bf8af6b
sanipop <thepersian82@gmail.com>
2023-12-02 00:43:21 +0000

    w

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,92 +1,32 @@
 #include "hash_tables.h"
 
 /**
- * set_pair - mallocs a key/value pair to the hash table.
- * @key: the key, a string that cannot be empty.
- * @value: the value associated with the key, can be an empty string.
- *
- * Return: pointer to the new node.
- */
-hash_node_t *set_pair(const char *key, const char *value)
-{
-	hash_node_t *node = malloc(sizeof(hash_node_t));
-
-	if (node == NULL)
-		return (NULL);
-	node->key = malloc(strlen(key) + 1);
-	if (node->key == NULL)
-		return (NULL);
-	node->value = malloc(strlen(value) + 1);
-	if (node->value == NULL)
-		return (NULL);
-	strcpy(node->key, key);
-	strcpy(node->value, value);
-	return (node);
-}
-
-/**
- * set_pair_only - (no collision) set key:value pair to first array element
- * @ht: pointer to the hash table.
- * @key: the key, a string that cannot be empty.
- * @value: the value associated with the key, can be an empty string.
- * @index: the key's index.
- *
- * Return: the node, or NULL if failed.
- */
-int set_pair_only(hash_table_t *ht, const char *key,
-		  const char *value, unsigned long int index)
-{
-	hash_node_t *node = set_pair(key, value);
-
-	if (node == NULL)
-		return (0);
-	node->next = NULL;
-	ht->array[index] = node;
-	return (1);
-}
-
-/**
- * hash_table_set - adds an element to the hash table.
+ * hash_table_get - retrieves a value associated with a key.
  * @ht: a pointer to the hash table array.
  * @key: the key, a string that cannot be empty.
- * @value: the value associated with the key, can be an empty string.
  *
- * Return: 1 on success, 0 on error.
+ * Return: the value associated with the key, or NULL if key can't be found.
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	hash_node_t *node;
 
-	if (key == NULL || ht == NULL)
-		return (0);
+	if (ht == NULL)
+		return (NULL);
+	if (key == NULL)
+		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
+	if (ht->array[index] == NULL)
+		return (NULL);
+	if (strcmp(ht->array[index]->key, key) == 0)
+		return (ht->array[index]->value);
 	node = ht->array[index];
-	if (node == NULL)
-		return (set_pair_only(ht, key, value, index));
 	while (node != NULL)
 	{
 		if (strcmp(node->key, key) == 0)
-		{
-			if (strcmp(node->value, value) == 0)
-				return (1);
-			free(node->value);
-			node->value = malloc(strlen(value) + 1);
-			if (node->value == NULL)
-				return (0);
-			strcpy(node->value, value);
-			return (1);
-		}
+			return (node->value);
 		node = node->next;
 	}
-	if (node == NULL)
-	{
-		node = set_pair(key, value);
-		if (node == NULL)
-			return (0);
-		node->next = ht->array[index];
-		ht->array[index] = node;
-		return (1);
-	}
-	return (0);
+	return (NULL);
 }
