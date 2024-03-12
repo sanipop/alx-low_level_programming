8283f2b4a1fda5dd007576228dc4596dcb24678c
sanipop <67932273+sanipop@users.noreply.github.com>
2024-03-12 12:27:10 +0100

    Create 104-advanced_binary.c

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,64 @@
+#include "search_algos.h"
+
+/**
+ * rec_search - search the value in array
+ * using the Binary search algorithm
+ *
+ *
+ * @array: the array to be searched
+ * @size: the size of input array
+ * @value: the value to search for
+ * Return: the value searched for
+ */
+int rec_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+	{
+		if (half > 0)
+			return (rec_search(array, half + 1, value));
+		return ((int)half);
+	}
+
+	if (value < array[half])
+		return (rec_search(array, half + 1, value));
+
+	half++;
+	return (rec_search(array + half, size - half, value) + half);
+}
+
+/**
+ * advanced_binary - the adv search
+ *
+ *
+ * @array: input array
+ * @size: the size of the input array
+ * @value: the value to search for
+ * Return: the value searched for
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	int index;
+
+	index = rec_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
+		return (-1);
+
+	return (index);
+}
